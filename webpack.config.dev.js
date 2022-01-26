18e5056ec6083dad4c2ab438e288f2ecfcfb0343
tim-graupe <tgraupe90@gmail.com>
2022-01-26 18:37:48 -0500

    Webpack

---
+++ webpack.config.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
