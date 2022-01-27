9479373f530e450f0cecff3aff9296879cb356d6
tim-graupe <tgraupe90@gmail.com>
2022-01-27 17:24:05 -0500

    Fixing webpack

--- src/dist/index.dev.js
+++ src/dist/index.dev.js
@@ -1,3 +1,13 @@
 "use strict";
 
-var _createProject = require("./createProject");
+var _createProject = require("./createProject.js");
+
+var _createTask = require("./createTask.js");
+
+var _manageTasks = require("./manageTasks.js");
+
+(0, _createTask.createNewTask)();
+(0, _createProject.createProject)();
+(0, _manageTasks.manageTasks)();
+(0, _manageTasks.savedTasks)(); //todo:  2) change due date, priority, description and title
+//known issues: mobile design needs improvement, refreshing page clears current project
--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import { createProject } from "./createProject";
-
+import {  createProject, currentProject, projects } from "./createProject.js";
+import { Task, createNewTask } from "./createTask.js";
+import { manageTasks, savedTasks } from "./manageTasks.js";
+createNewTask()
+createProject()
+manageTasks()
+savedTasks()
 //todo:  2) change due date, priority, description and title
 //known issues: mobile design needs improvement, refreshing page clears current project
