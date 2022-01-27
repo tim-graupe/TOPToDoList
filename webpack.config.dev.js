f515e963aab9662222b3219184758542024c99a0
tim-graupe <tgraupe90@gmail.com>
2022-01-27 17:31:55 -0500

    hjjkn;

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,7 +3,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: '../src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
