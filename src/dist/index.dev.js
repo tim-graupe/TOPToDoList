177b11c3f362eae99b800d262addce55d6a5d77c
tim-graupe <tgraupe90@gmail.com>
2022-01-27 18:43:18 -0500

    Changing path

--- src/dist/index.dev.js
+++ src/dist/index.dev.js
@@ -1,13 +1,12 @@
 "use strict";
 
-var _createProject = require("./createProject.js");
+var _srccreateProject = require("srccreateProject.js");
 
-var _createTask = require("./createTask.js");
+var _srcindex = require("srcindex.js");
 
-var _manageTasks = require("./manageTasks.js");
+var _srcmanageTasks = require("srcmanageTasks.js");
 
-(0, _createTask.createNewTask)();
-(0, _createProject.createProject)();
-(0, _manageTasks.manageTasks)();
-(0, _manageTasks.savedTasks)(); //todo:  2) change due date, priority, description and title
+(0, _srcindex.createNewTask)();
+(0, _srccreateProject.createProject)();
+(0, _srcmanageTasks.manageTasks)(); //todo:  2) change due date, priority, description and title
 //known issues: mobile design needs improvement, refreshing page clears current project
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import {  createProject, currentProject, projects } from "./createProject.js";
-import { Task, createNewTask } from "./createTask.js";
-import { manageTasks, savedTasks } from "./manageTasks.js";
+import {  createProject, currentProject, projects } from "src\createProject.js";
+import { Task, createNewTask } from "src\index.js";
+import { manageTasks, savedTasks } from "src\manageTasks.js";
 createNewTask()
 createProject()
 manageTasks()
-savedTasks()
 //todo:  2) change due date, priority, description and title
 //known issues: mobile design needs improvement, refreshing page clears current project
